cedde859fee45095e624842e7a182199f9756a3e
Brian Scaturro <scaturrob@gmail.com>
2015-03-08 18:55:56 -0400

    test choice store filter

--- src/scripts/stores/__tests__/ChoiceStore-test.js
+++ src/scripts/stores/__tests__/ChoiceStore-test.js
@@ -1,17 +1,27 @@
+jest.dontMock('../../constants/ChoiceConstants');
 jest.dontMock('../ChoiceStore');
 jest.dontMock('lodash');
 jest.dontMock('../../data.json');
+jest.dontMock('react/lib/keyMirror');
 
 describe('ChoiceStore', function () {
 
+  var ChoiceConstants = require('../../constants/ChoiceConstants');
   var ChoiceStore;
   var AppDispatcher;
   var data;
+  var callback;
+
+  var filterAction = {
+    actionType: ChoiceConstants.CHOICE_FILTER,
+    search: 'satay'
+  };
 
   beforeEach(function () {
     AppDispatcher = require('../../dispatcher/AppDispatcher');
     ChoiceStore = require('../ChoiceStore');
     data = require('../../data.json');
+    callback = AppDispatcher.register.mock.calls[0][0];
   });
 
   it('registers a callback with the dispatcher', function () {
@@ -52,6 +62,13 @@ describe('ChoiceStore', function () {
       var all = ChoiceStore.getChoices();
       expect(all.length).toEqual(data.mealOptions.length);
     });
+  });
+
+  it('filters choices', function () {
+    callback(filterAction);
+    var choices = ChoiceStore.getChoices();
+    expect(choices.length).toEqual(1);
+    expect(choices[0].name).toEqual('Satay Chicken');
   });
 
 });
